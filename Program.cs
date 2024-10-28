1a364e5792564e21764e5c4a391bb190ec71207a
Kevin Djidoda <118570461+kdjidoda@users.noreply.github.com>
2024-10-28 13:24:23 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string[] s2 = s1.Split('');
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+        
+        }
+    }
+}
